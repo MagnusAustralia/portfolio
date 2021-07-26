44d621b6b917ddf2722340ebd8854b365afdeb92
williamcool7890 <62357797+williamcool7890@users.noreply.github.com>
2021-07-26 21:33:56 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import Layout from "../components/Layout";
-<<<<<<< HEAD
 import "../styles.css";
-=======
 import "../styles/globals.css";
->>>>>>> 7adc71e831af742df9f4555e1d1f39c271b90be2
 
 export default function MyApp({ Component, pageProps }) {
 	return (
