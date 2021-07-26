7adc71e831af742df9f4555e1d1f39c271b90be2
williamcool7890 <62357797+williamcool7890@users.noreply.github.com>
2021-07-26 17:43:13 +0100

    Nav Included

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,22 @@
 import Link from "next/link";
+import styles from "../styles/Nav.module.css";
 
 const Nav = () => {
 	return (
 		<nav>
-			<ul>
-				<li>
-					<Link href="/">Home</Link>
-				</li>
-				<li>
-					<Link href="/learn">Learn</Link>
-				</li>
-				<li>
-					<Link href="/records">Records</Link>
-				</li>
-			</ul>
+			<div className={styles.nav}>
+				<ul>
+					<li>
+						<Link href="/">Home</Link>
+					</li>
+					<li>
+						<Link href="/learn">Learn</Link>
+					</li>
+					<li>
+						<Link href="/records">Records</Link>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
 	return (
-		<section className={styles.section}>
-			<h1 className={styles.header}>Hello World</h1>
+		<section>
+			<h1>Hello World</h1>
 		</section>
 	);
 }
