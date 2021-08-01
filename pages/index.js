3751b65d1342600a1882767cc24719740b165f0d
williamcool7890 <62357797+williamcool7890@users.noreply.github.com>
2021-08-01 09:35:09 +0100

    New Link added

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ export default function Home() {
 			<a href="http://learn-numbers.william-portfolio.design">
 				Check out my site that helps learning numbers
 			</a>
+			<a href="http://slightlyuseful.william-portfolio.design">
+				Check out my site for some slightly useful things
+			</a>
 		</div>
 	);
 }
