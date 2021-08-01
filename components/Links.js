0fd387cfa6b9d0512ff0c4acf8864027cad78e6b
williamcool7890 <62357797+williamcool7890@users.noreply.github.com>
2021-08-01 09:51:09 +0100

    New Link added

--- components/Links.js
+++ components/Links.js
@@ -5,13 +5,15 @@ const Links = () => {
 	return (
 		<div className={styles.container}>
 			<h1>Portfolio Home</h1>
-			<a href="http://learn-numbers.william-portfolio.design">
-				Check out my site that helps learning numbers
-			</a>
-			<br />
-			<a href="http://slightlyuseful.william-portfolio.design">
-				Check out my site for some slightly useful things
-			</a>
+			<p>
+				<a href="http://learn-numbers.william-portfolio.design">
+					Check out my site that helps learning numbers
+				</a>
+				<br />
+				<a href="http://slightlyuseful.william-portfolio.design">
+					Check out my site for some slightly useful things
+				</a>
+			</p>
 		</div>
 	);
 };
